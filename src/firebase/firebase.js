4605a51785f04fc4507673640e4efbca2bb60790
ismail debbarh <usamdh07@gmail.com>
2023-01-29 19:24:06 +0100

    edit firebase object

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,15 +4,14 @@ import { GoogleAuthProvider } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLvw7lVZMUj6ZBDt0y_SvivuJlMIdWUQM",
-  authDomain: "slack-app-clone-r.firebaseapp.com",
-  projectId: "slack-app-clone-r",
-  storageBucket: "slack-app-clone-r.appspot.com",
-  messagingSenderId: "862995554263",
-  appId: "1:862995554263:web:6bb281abeb5cd3c6d8f592",
-  measurementId: "G-ETH3EGYZ9V"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
